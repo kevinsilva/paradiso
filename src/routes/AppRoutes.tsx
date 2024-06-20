2a089ec881214cfc2dd3d5f91c2f9ea880879484
kevinsilva <kevinsilva.j@gmail.com>
2024-06-20 18:19:16 +0100

    [feat]: adds AppRoutes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import { SearchPage } from '../pages/SearchPage';
+import { HomePage } from '../pages/HomePage';
+import { MoviePage } from '../pages/MoviePage';
+import { SeriePage } from '../pages/SeriePage';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/search/:query' element={<SearchPage />} />
+      <Route path='/movie/:id/:movieName' element={<MoviePage />} />
+      <Route path='/tvSerie/:id/:serieName' element={<SeriePage />} />
+    </Routes>
+  );
+};
