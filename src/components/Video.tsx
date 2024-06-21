405cdb5dff3a8f5fe9362e8cb7648895680d001b
kevinsilva <kevinsilva.j@gmail.com>
2024-06-21 16:53:36 +0100

    [feat]: adds video component

---
+++ src/components/Video.tsx
@@ -0,0 +1,34 @@
+import { useRef } from 'react';
+import { VIDEO_URL } from '../utils/constants';
+
+export const Video = (sourceURL: string) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const handlePlay = () => {
+    const videoElement = videoRef.current;
+
+    if (videoElement) {
+      if (videoElement.requestFullscreen) {
+        videoElement.requestFullscreen();
+      } else if ((videoElement as any).videoElement.webkitRequestFullscreen) {
+        (videoElement as any).webkitRequestFullscreen();
+      } else if ((videoElement as any).videoElement.msRequestFullscreen) {
+        (videoElement as any).videoElement.msRequestFullscreen();
+      }
+    }
+  };
+
+  return (
+    <div className='rounded-lg'>
+      <video
+        className='absolute inset-0 h-full w-full'
+        controls
+        autoPlay
+        ref={videoRef}
+        onPlay={handlePlay}
+      >
+        <source src={sourceURL || VIDEO_URL} type='video/mp4' />
+      </video>
+    </div>
+  );
+};
