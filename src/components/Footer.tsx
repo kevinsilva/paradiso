21ddca1475dbe6835b9d0c6ac031f549105e5b7d
kevinsilva <kevinsilva.j@gmail.com>
2024-06-22 18:00:20 +0100

    [chore]: updates footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return (
-    <footer className='flex flex-col items-center justify-center text-zinc-500 text-sans mb-2 mt-auto pt-16'>
-      <small>
+    <footer className='text-sans mt-auto flex flex-col items-center justify-center bg-black pt-16 text-zinc-500'>
+      <small className='mb-4'>
         &copy;{new Date().getFullYear()} Kevin Silva.{' '}
         <span className='text-xs'>
-          Built with React & TypeScript, Tailwind CSS. Data from TMDB
+          Built with React & TypeScript, Tailwind CSS. Data from{' '}
+          <Link to={'https://www.themoviedb.org/'} className='hover:opacity-90'>
+            TMDB
+          </Link>
         </span>
       </small>
     </footer>
