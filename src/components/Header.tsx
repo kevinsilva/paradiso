712fd53e07ed8bf1d80df392e0b948c8e35ae1cc
kevinsilva <kevinsilva.j@gmail.com>
2024-06-22 15:32:39 +0100

    [style]: styles header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { KeyboardEvent } from 'react';
+import { useState, KeyboardEvent } from 'react';
 import { MagnifyingGlass } from '@phosphor-icons/react';
 import { useNavigate } from 'react-router-dom';
 
@@ -23,12 +22,12 @@ export const Header = () => {
   };
 
   return (
-    <header className='absolute left-0 top-0 z-50 flex w-full items-center justify-between p-4 text-white'>
-      <h1 className='text-center text-2xl font-bold text-black'>Paradiso</h1>
+    <header className='absolute left-0 top-0 z-50 flex w-full items-center justify-between p-8 text-white'>
+      <h1 className='text-center text-2xl font-bold text-white'>Paradiso</h1>
       <div className='flex items-center'>
         <input
           type='text'
-          className={`px-2 py-1 text-black transition-all duration-300 ${
+          className={`rounded px-2 py-1 text-black transition-all duration-300 focus:outline-none ${
             showInput ? 'w-40' : 'w-0 opacity-0'
           }`}
           placeholder='Search...'
@@ -38,7 +37,7 @@ export const Header = () => {
           size={24}
           weight='bold'
           onClick={toggleInput}
-          className='ml-2 cursor-pointer text-black'
+          className='ml-2 cursor-pointer text-white'
         />
       </div>
     </header>
