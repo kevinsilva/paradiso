72e4e8672ca14f0553576cb290e2cc7fcd6662e0
kevinsilva <kevinsilva.j@gmail.com>
2024-06-22 17:38:17 +0100

    [chore]: adds card types

--- src/components/HorizontalCard.tsx
+++ src/components/HorizontalCard.tsx
@@ -1,28 +1,28 @@
-type HorizontalCardTypes = {
-  title: string;
-  imageSrc: string;
-  genre?: string;
-};
+import { Link } from 'react-router-dom';
+import { CardTypes } from '../utils/types';
+import {
+  HORIZONTAL_CARD_SIZE,
+  CARD_PARAGRAPH_HEIGHT,
+  IMAGE_URL,
+} from '../utils/constants';
 
-export const HorizontalCard = ({
-  title,
-  imageSrc,
-  genre,
-}: HorizontalCardTypes) => {
+export const HorizontalCard = ({ title, imageSrc, link }: CardTypes) => {
   return (
-    <div className='group relative flex h-[10.438rem] w-[18.563rem] cursor-pointer flex-col'>
-      <img
-        src={imageSrc}
-        className='h-full w-full rounded-xl object-cover transition-opacity duration-300 group-hover:opacity-75'
-      ></img>
-      {genre && (
-        <div className='absolute left-2 top-2 z-10'>
-          <span className='inline-block rounded-full bg-white bg-opacity-75 px-2 py-1 text-sm'>
-            {genre}
-          </span>
-        </div>
-      )}
-      <p className='mt-1 text-lg font-bold'>{title}</p>
-    </div>
+    <Link to={link || ''}>
+      <div
+        className={`group relative mx-2 flex h-[calc(${HORIZONTAL_CARD_SIZE.height} + ${CARD_PARAGRAPH_HEIGHT})] w-[${HORIZONTAL_CARD_SIZE.width}] mb-8 cursor-pointer flex-col`}
+      >
+        <img
+          src={IMAGE_URL + imageSrc}
+          className={`h-[${HORIZONTAL_CARD_SIZE.height}] w-full rounded-xl object-cover transition-opacity duration-300 group-hover:opacity-75`}
+        ></img>
+
+        <p
+          className={`text-md mt-1 h-[${CARD_PARAGRAPH_HEIGHT}] font-bold text-white`}
+        >
+          {title}
+        </p>
+      </div>
+    </Link>
   );
 };
--- src/components/VerticalCard.tsx
+++ src/components/VerticalCard.tsx
@@ -1,17 +1,23 @@
-type VerticalCardTypes = {
-  title: string;
-  imageSrc: string;
-};
+import { Link } from 'react-router-dom';
+import { CardTypes } from '../utils/types';
+import { CARD_PARAGRAPH_HEIGHT, VERTICAL_CARD_SIZE } from '../utils/constants';
 
-export const VerticalCard = ({ title, imageSrc }: VerticalCardTypes) => {
+export const VerticalCard = ({ title, imageSrc, link }: CardTypes) => {
   return (
-    <div className='group relative flex h-[27.813rem] w-[18.563rem] cursor-pointer flex-col'>
-      <img
-        src={imageSrc}
-        className='h-full min-h-[27.813rem] w-full rounded-xl object-cover transition-all duration-300 group-hover:contrast-75'
-      ></img>
-      {/* <div className='absolute inset-0 rounded-xl bg-black/50 opacity-0 transition-opacity duration-300 group-hover:opacity-10'></div> */}
-      <p className='text-md mt-1 font-bold'>{title}</p>
-    </div>
+    <Link to={link || ''}>
+      <div
+        className={`group relative mx-2 flex h-[calc(${VERTICAL_CARD_SIZE.height} + ${CARD_PARAGRAPH_HEIGHT})] w-[${VERTICAL_CARD_SIZE.width}] cursor-pointer flex-col`}
+      >
+        <img
+          src={imageSrc}
+          className={`h-[${VERTICAL_CARD_SIZE.height}] rounded-xl object-cover transition-all duration-300 group-hover:contrast-75`}
+        ></img>
+        <p
+          className={`text-md mt-1 h-[${CARD_PARAGRAPH_HEIGHT}] font-bold text-white`}
+        >
+          {title}
+        </p>
+      </div>
+    </Link>
   );
 };
