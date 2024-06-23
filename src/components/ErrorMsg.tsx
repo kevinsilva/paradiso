bbc79d05808a0d7e56336940cd84379796b82c9b
kevinsilva <kevinsilva.j@gmail.com>
2024-06-23 13:38:15 +0100

    [feat]: adds error message

---
+++ src/components/ErrorMsg.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { WarningCircle } from '@phosphor-icons/react';
+
+export const ErrorMsg = ({ text }: { text: string }) => {
+  return (
+    <div className='flex items-center justify-center text-red-700'>
+      <WarningCircle size={32} />
+      {text}
+    </div>
+  );
+};
